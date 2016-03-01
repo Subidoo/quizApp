4c35fd9b0880de7818bbbfdf7301821c86ed2af0
Kyle Prestgard <ktprestgard@gmail.com>
2016-02-29 19:37:00 -0700

    more stuff

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
 angular.module('quizApp', ['ui.router']);
 
-angular.module('quizApp').config([
-	'$stateProvider', 
-	'$urlRouterProvider', 
+angular.module('quizApp', [
+        'ui.router'
+    ])
+    .run(function ($rootScope, $state) {        
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+            if(toState.name === 'quiz') {
+                event.preventDefault();
+                $state.go('quiz.view', toParams)
+            }
+        })
+    })
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-	function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
 
-		$urlRouterProvider.otherwise('/home');
-
-		$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl: './components/home/homeView.html',
-				controller: 'homeCtrl',
-				 resolve: {
-        			quizList: function (quizService) {
-          			return quizService.getQuizNames();
-        		}
+        $stateProvider
+           .state('home', {
+			url: '/',
+			templateUrl: 'components/home/homeView.html',
+			controller: 'homeCtrl',
+			resolve: {
+				quizList: function (quizService) {
+					return quizService.getQuizNames();
+				}
 			}
 		})
-
-			.state('quiz', {
-				url: '/quiz',
-				templateUrl: './components/quiz/quizView.html',
-				controller: 'quizCtrl'
-			})
-
-			.state('results', {
-				url: '/results',
-				templateUrl: './components/results/resultsView.html',
-				controller: 'resultsCtrl'
-			})
-
-}]);
+        .state('quiz', {
+            url: '/quiz/:quizName',
+            templateUrl: 'components/quiz/views/quizContainerView.html',
+            controller: 'quizCtrl',
+            resolve: {
+                questions: function (quizService, $stateParams) {
+                    var name = $stateParams.quizName
+                    return quizService.getQuestions(name);
+                }
+            }
+        })
+        .state('quiz.view', {
+            parent: 'quiz',
+            views: {
+                'list': {
+                    templateUrl: 'components/quiz/views/questionListWrapperView.html'
+                },
+                'detail': {
+                    templateUrl: 'components/quiz/views/questionDetailView.html'
+                }
+            }
+        })
+    });
